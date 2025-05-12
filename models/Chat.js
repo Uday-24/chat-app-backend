7622bbc58ec3494aba70f91ca9db16c4a3b16f74
Uday-24 <chotaliyauday24@gmail.com>
2025-05-12 17:21:56 +0530

    Chat and Message models

---
+++ models/Chat.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const chatSchema = mongoose.Schema({
+    isGroupChat: {
+        type: Boolean,
+        default: false
+    },
+    chatName: {
+        type: String,
+        trim: true,
+    },
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ],
+    latestMessage: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Message',
+    },
+    groupAdmin: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Chat', chatSchema);
---
+++ models/Message.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const messageSchema = mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    content: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    chat: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Chat',
+        required: true,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Message', messageSchema);
